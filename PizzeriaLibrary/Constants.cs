1a8d010c483f163a0a96b633a6c7f4a0490b572b
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-09 15:20:45 +0200

    Refactoring

--- PizzeriaLibrary/Constants.cs
+++ PizzeriaLibrary/Constants.cs
@@ -3,5 +3,6 @@
     public static class Constants
     {
         public static string DIR_PATH = @"C:\Users\student\Desktop\Pizza order";
+        public static string WRITE_DIR_PATH = @"C:\Users\student\Desktop\Pizza order";
     }
 }
