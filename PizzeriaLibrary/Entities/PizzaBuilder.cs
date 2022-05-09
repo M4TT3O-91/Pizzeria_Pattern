950bcaf7392a601ef50c60387ddc33d4e97f8c51
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-09 11:43:56 +0200

    Pizza builder

---
+++ PizzeriaLibrary/Entities/PizzaBuilder.cs
@@ -0,0 +1,45 @@
+﻿namespace PizzeriaLibrary.Entities
+{
+    public class PizzaBuilder
+    {
+        private PizzaOrder _pizza;
+        private PizzaBuilder()
+        {
+            _pizza = new PizzaOrder();
+        }
+
+        public static PizzaBuilder Create()
+        {
+            return new PizzaBuilder();
+        }
+
+        public PizzaBuilder WithPizzaType(string pizzaType)
+        {
+            _pizza.BasePizza = pizzaType;
+            return this;
+        }
+        public PizzaBuilder WithImpastoType(string impasto)
+        {
+            _pizza.Impasto = impasto;
+            return this;
+        }
+
+        public PizzaBuilder WithIngredients(string ingredientsList)
+        {
+            if(ingredientsList?.Any() != true)
+            {
+                _pizza.Ingredients = new List<string>();
+            }
+
+            _pizza.Ingredients.Add(ingredientsList);
+            return this;
+        }
+
+        public PizzaOrder Build()
+        {
+            return _pizza;
+        }
+
+
+    }
+}
